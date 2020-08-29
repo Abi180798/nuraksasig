85f6fa640e5b487bac51832ff133492f4c66fc02
Abi180798 <habiburrahman180798@gmail.com>
2020-08-29 12:22:22 +0800

    change css

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
+import '../static/css/styles.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../static/styles.scss'
-import '../static/css/styles.css'
 
 export default function MyApp({ Component, PageProps}) {
     return <Component {...PageProps} />
