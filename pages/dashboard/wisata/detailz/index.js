32d894e4c8041d1ae04512e3ece1d2651ff01af3
Abi180798 <habiburrahman180798@gmail.com>
2020-08-30 14:40:07 +0800

    changev3

--- pages/dashboard/wisata/detailz/index.js
+++ pages/dashboard/wisata/detailz/index.js
@@ -1,20 +1,20 @@
 import Layout from "../../../layouts/Layout";
 import FormWisata from "../components/FormWisata";
 import dataWisata from '../../../../mock/wisata.json'
-import { useRouter } from './node_modules/next/router'
+import { useRouter } from 'next/router'
 
-export default function Edit(){
+export default function Detailz(){
     const router = useRouter()
-
+    
     return(
         <Layout title="Wisata">
             <main>
                 <div className="container-fluid">
-                    <h1 className="mt-4">Edits Wisata</h1>
+                    <h1 className="mt-4">Details Wisata</h1>
                     <ol className="breadcrumb mb-4">
                         <li className="breadcrumb-item">Manajemen Data</li>
                         <li className="breadcrumb-item">Wisata</li>
-                        <li className="breadcrumb-item active" aria-current="page">Edit</li>
+                        <li className="breadcrumb-item active" aria-current="page">Detail</li>
                     </ol>
                     <div className="card">
                         <div className="card-body ml-5 mr-5">
